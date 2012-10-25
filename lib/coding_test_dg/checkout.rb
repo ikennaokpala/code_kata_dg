4a99b52cc9e6a9feb75f88115fd41823553348cf
Ikenna Okpala <ikennaokpala@gmail.com>
2012-10-25 03:17:54 +0100

    Checkout spect and implementation

--- lib/coding_test_dg/checkout.rb
+++ lib/coding_test_dg/checkout.rb
@@ -1,7 +1,15 @@
 class Checkout
+  attr_accessor :cart, :pricing_rules
   def initialize(pricing_rules)
+    @pricing_rules = pricing_rules
+    @cart = []
   end
 
   def scan(item)
+    @cart << item
+  end
+
+  def total
+    @pricing_rules.apply(self)
   end
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,47 @@
 require 'spec_helper'
 
 describe Checkout do
-  its(:total){ should == 22.45}
-  its(:total){ should == 3.11}
-  its(:total){ should == 16.61}
+  before :each do
+    @pricing_rules = Rule::Pricing.new
+  end
+
+  it "its price should be 22.45 " do
+    item1 = Product.new("FR1", 3.11)
+    item2 = Product.new("SR1", 5.00)
+    item3 = Product.new("FR1", 3.11)
+    item4 = Product.new("FR1", 3.11)
+    item5 = Product.new("CF1", 11.23)
+    co = Checkout.new(@pricing_rules)
+    co.scan(item1)
+    co.scan(item2)
+    co.scan(item3)
+    co.scan(item4)
+    co.scan(item5)
+    price = co.total
+    price.should == 22.45
+  end
+
+  it "returns the price of the checkout" do
+    item1 = Product.new("FR1", 3.11)
+    item2 = Product.new("FR1", 3.11)
+    co = Checkout.new(@pricing_rules)
+    co.scan(item1)
+    co.scan(item2)
+    price = co.total
+    price.should == 3.11
+  end
+
+  it "returns the price of the checkout" do
+    item1 = Product.new("SR1", 5.00)
+    item2 = Product.new("SR1", 5.00)
+    item3 = Product.new("FR1", 3.11)
+    item4 = Product.new("SR1", 5.00)
+    co = Checkout.new(@pricing_rules)
+    co.scan(item1)
+    co.scan(item2)
+    co.scan(item3)
+    co.scan(item4)
+    price = co.total
+    price.should == 16.61
+  end
 end
