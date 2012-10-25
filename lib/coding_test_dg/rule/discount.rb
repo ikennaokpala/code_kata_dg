2151b102485ca1bdabd068f8a86654c06fb318e7
Ikenna Okpala <ikennaokpala@gmail.com>
2012-10-25 05:26:11 +0100

    Return 0 when condition is not meet

--- lib/coding_test_dg/rule/discount.rb
+++ lib/coding_test_dg/rule/discount.rb
@@ -1,6 +1,6 @@
 module Rule
   class Discount
-    DEFAULT_RULE = {product_code:"SR1", qty:3, price:4.50}
+    DEFAULT_RULE = {product_code:"SR1", qty:3, price:5.00, rate:0.1}
     def initialize(rule={})
       @rule = DEFAULT_RULE.merge(rule)
     end
@@ -8,9 +8,9 @@ module Rule
     def apply(checkout)
       product_count = checkout.cart.map(&:product_code).count(@rule.fetch(:product_code))
       if product_count >= @rule.fetch(:qty)
-        @rule.fetch(:price) * product_count
+        (@rule.fetch(:price) * @rule.fetch(:rate)) * product_count
       else
-        checkout.cart.map(&:price).first * product_count
+        0
       end
     end
   end
