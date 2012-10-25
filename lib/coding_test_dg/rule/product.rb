afaebb94801ff5af5983c9de650508af4c92974c
Ikenna Okpala <ikennaokpala@googlemail.com>
2012-10-25 08:23:17 +0200

    Make slice each value configurable to accommodate 3 for 1

--- lib/coding_test_dg/rule/product.rb
+++ lib/coding_test_dg/rule/product.rb
@@ -7,7 +7,7 @@ module Rule
 
     def apply(checkout)
       ft_products = checkout.cart.select(&:product_code).select{|p| p.product_code == @rule.fetch(:product_code)}
-      free_products = ft_products.each_slice(2).select{|p| p.size == 2}.count
+      free_products = ft_products.each_slice(@rule.fetch(:qty)).select{|p| p.size == 2}.count
       @rule.fetch(:price) * free_products
     end
   end
