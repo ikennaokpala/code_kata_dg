b9e1f3cbcb42e2e9f62a805a72ba4a50cb6bb1a4
Ikenna Okpala <ikennaokpala@googlemail.com>
2012-10-25 08:28:00 +0200

    Append @sign

--- lib/coding_test_dg/rule/product.rb
+++ lib/coding_test_dg/rule/product.rb
@@ -7,7 +7,7 @@ module Rule
 
     def apply(checkout)
       ft_products = checkout.cart.select(&:product_code).select{|p| p.product_code == @rule.fetch(:product_code)}
-      free_products = ft_products.each_slice(@rule.fetch(:qty)).select{|p| p.size == rule.fetch(:qty)}.count
+      free_products = ft_products.each_slice(@rule.fetch(:qty)).select{|p| p.size == @rule.fetch(:qty)}.count
       @rule.fetch(:price) * free_products
     end
   end
